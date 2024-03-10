bb46356b9305bbbc4bb9f0c0cf41e8c42fc7e484
VinhNguyenD23 <nguyenthanhvinh31305@gmail.com>
2024-03-10 13:10:48 +0700

    không code trong App.jsx hoặc main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/App.css";
 import UI from "./Pages/Login";
-import PageNotFound from "./Pages/404";
 import { React } from "react";
 
 export default function App() {
-  {/*return <UI />;*/}
-  return <PageNotFound/>
+  return <UI />;
 }
