b9eb3c9da9309defd8464e77cd4685116f83583d
ngthanhhhh <nguyetthanh225@gmail.com>
2024-03-10 11:38:38 +0700

    Edit 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/App.css";
 import UI from "./Pages/Login";
+import PageNotFound from "./Pages/404";
 import { React } from "react";
 
 export default function App() {
-  return <UI />;
+  {/*return <UI />;*/}
+  return <PageNotFound/>
 }
